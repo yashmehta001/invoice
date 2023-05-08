80d3286e94c19a4443ef6e38711eb664ff06653e
Yash Mehta <yashmehta766@gmail.com>
2023-05-08 18:45:20 +0530

    Revert "connected DB"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: String(process.env.DB_PASSWORD),
-      database: process.env.DB_DATABASE,
-      autoLoadEntities: true,
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
