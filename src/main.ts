712387e7c930cf4a2a40002d4ee8836f291db1b4
yashmehta001 <yash.rajesh@deepcoder.in>
2023-05-08 18:38:41 +0530

    connected DB

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3000, () => {
+    console.log(`server is running at port ${process.env.PORT}`);
+  });
 }
 bootstrap();
