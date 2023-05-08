38745ac9c033078da52b091c94c5a5adda702ed6
Yash Mehta <yashmehta766@gmail.com>
2023-05-08 19:31:33 +0530

    Revert "connected DB"

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000, () => {
-    console.log(`server is running at port ${process.env.PORT}`);
-  });
+  await app.listen(3000);
 }
 bootstrap();
